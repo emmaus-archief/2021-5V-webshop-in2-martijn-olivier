530b6fc10943b86afa23a1a677e169848ad33357
Olivier Drees <olivier.s.drees@gmail.com>
2020-11-23 11:17:54 +0000

    test commit ('French film' aangepast naar 'Blauwe trui' als test)

--- sql/seed.sql
+++ sql/seed.sql
@@ -5,7 +5,7 @@
 --
 
 
-insert into products (name, description, code, price) values ('French Film', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+insert into products (name, description, code, price) values ('Blauwe Trui', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
 
 Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
 
