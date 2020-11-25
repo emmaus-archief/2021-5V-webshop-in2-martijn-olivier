9054bc012afb73d012494111b031c68ef64d04a1
Olivier Drees <olivier.s.drees@gmail.com>
2020-11-25 14:03:59 +0000

    nieuwe tabellen gemaakt en products tabel aangepast.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,25 @@
 DROP TABLE IF EXISTS products; 
 CREATE TABLE products (
   ID SERIAL PRIMARY KEY,
-  code VARCHAR(15),
-  name VARCHAR(255),
+  product_naam VARCHAR(15),
+  merk_id VARCHAR(255),
+  maat_id VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
+  kleur_id VARCHAR(255),
+  prijs NUMERIC(10, 2)
+);
+
+CREATE TABLE merken (
+  ID SERIAL PRIMARY KEY,
+  merk VARCHAR(30)
+);
+
+CREATE TABLE maten (
+  ID SERIAL PRIMARY KEY,
+  maat VARCHAR(30)
+);
+
+CREATE TABLE kleuren (
+  ID SERIAL PRIMARY KEY,
+  kleur VARCHAR(30)
 );
