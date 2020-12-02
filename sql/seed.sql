30adf2cb397d5ffca4f32146ae27d84a0516bd4d
Olivier Drees <olivier.s.drees@gmail.com>
2020-12-02 15:09:34 +0000

    truien toegevoegd

--- sql/seed.sql
+++ sql/seed.sql
@@ -26,6 +26,7 @@ insert into kleuren (kleur) values ("Zwart");
 insert into kleuren (kleur) values ("Wit");
 insert into kleuren (kleur) values ("Rood");
 insert into kleuren (kleur) values ("Blauw");
+insert into kleuren (kleur) values ("Grijs");
 
 -- Kledingtypes
 insert into kledingtype (type) values ("Trui");
@@ -34,23 +35,23 @@ insert into kledingtype (type) values ("Shirt");
 
 
 /* insert producten */
-
+-- Truien
 -- 01
 insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Zwarte Nike sweater', 1, 2, 2, 1, 45, "Zwarte Nike sweater met groot wit logo op de borst."); -- https://www.wehkamp.nl/nike-sweater-zwart-16303993/
 -- 02
-insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', "2", "1", "1", 1, "Mooie trui", 15);
+insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Zwarte Adidas Hoodie (core18)', 2, 2, 2, 1, 30, "Sweater met capouchon en kangoeroezak, gemaakt van katoen met polyester."); -- https://www.yoursportshop.nl/adidas-core18-hoody/?color=4322&size=8&gclid=Cj0KCQiAk53-BRD0ARIsAJuNhpt2DSReBFKit_u5CkhVkwoD98pkh0HoThOcuLrV7JiFxHDx01FTIwAaAnZdEALw_wcB
 -- 03
-insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', "3", "1", "1", 1, "Mooie trui", 15);
+insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Witte Stone Island hoodie', 3, 3, 3, 1, 150, "Hoodie met capouchon en kangoeroezak, gemaakt van 100% katoen."); -- https://www.debijenkorf.nl/-61640-basic-hoodie-met-merkapplicatie-5697040011-569704001111128?utm_source=google&utm_medium=organic&utm_campaign=organicshopping
 -- 04
-insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', "4", "1", "1", 1, "Mooie trui", 15);
+insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Grijze Puma hoodie essentials fleece', 4, 1, 6, 1, 15, "Puma hoodie met capouchon en kangoeroezak, gemaakt van katoen en polyester.");
 -- 05
-insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', "5", "1", "1", 1, "Mooie trui", 15);
+insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', 5, 3, 1, 1, 15, "Mooie trui");
 -- 06
-insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', "2", "1", "1", 1, "Mooie trui", 15);
+insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', 2, 2, 1, 1, 15, "Mooie trui");
 -- 07
-insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', "5", "1", "1", 1, "Mooie trui", 15);
+insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', 5, 2, 1, 1, 15, "Mooie trui");
 -- 08
-insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', "3", "1", "1", 1, "Mooie trui", 15);
+insert into products (product_naam, merk_id, maat_id, kleur_id, kledingtype_id, prijs, description) values ('Blauwe Trui', 3, 1, 1, 1, 15, "Mooie trui");
 
 -- Shirts
 -- 09
