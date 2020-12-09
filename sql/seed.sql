b76568bfd1fe2ce538b8746f63b8e65644ccf40c
Martijn5V <112191@emmauscollege.nl>
2020-12-09 14:20:20 +0000

    naam plaatjes verandert en de website werkt half

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,4 +1,4 @@
--- generared using
+ -- generared using
 -- curl 'https://api.mockaroo.com/api/910b6c20?count=100&key=90eac760' > seed.sql
 --
 -- want different data? check: https://www.mockaroo.com/910b6c20
